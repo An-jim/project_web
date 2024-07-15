127f009a9b1d556b2560604843523e6e1c75a61a
jim <jim>
2024-07-16 04:14:40 +0800

    多語系設定

---
+++ project_web/App_Start/LocalizationAttribute1.cs
@@ -0,0 +1,11 @@
+﻿xusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace project_web.App_Start
+{
+    public class LocalizationAttribute1
+    {
+    }
+}
